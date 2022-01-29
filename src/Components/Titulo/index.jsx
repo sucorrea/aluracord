a3c96bb80749525868993035f0f47386969a0ec2
sueli Corrêa de Oliveira <sueli.oliveira@newconsoftware.com.br>
2022-01-29 11:23:15 -0300

    Mudança de cores

---
+++ src/Components/Titulo/index.jsx
@@ -0,0 +1,18 @@
+import appConfig from "../../../config.json";
+
+export const Titulo = (props) => {
+  const Tag = props.tag || "h1";
+
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.neutrals["000"]};
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+};
